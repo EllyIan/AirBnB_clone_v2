456c22a05c24c4f7c84003d807208b63e8cdbda1
abey546 <abeyhassan546@gmail.com>
2024-03-19 02:12:28 -0700

    setting up the server

---
+++ setup_mysql_dev.sql
@@ -0,0 +1,12 @@
+-- this script prepares MySQL server 
+-- creates database named hbnb_dev_db
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+
+-- create new user with all the privilidges on db hbnb_dev_db
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+-- grant all priviledges to the new user
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
+FLUSH PRIVILEGES;
+-- granting the SELECT privilege for the user hbnb_dev in the db performance_schema
+GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
+FLUSH PRIVILEGES;
